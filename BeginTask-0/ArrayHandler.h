06e4701f6dc98d360dea66165f3c54d599402814
AlinaMiti <alinamitina277@gmail.com>
2024-10-02 13:45:50 +0300

    hg

--- BeginTask-0/ArrayHandler.h
+++ BeginTask-0/ArrayHandler.h
@@ -1,16 +1,34 @@
+#include <cstdint>
+#include <cstring>
+#include <iostream>
 
 template<typename T>
 class ArrayHandler {
 private:
     size_t _size;
     T* _array;
+    long long int _count;
+    T _max;
+    T _min;
 public:
     ArrayHandler(size_t size = 10) {
-
+        _size = size;
+        _array = new T[_size];
+        _max = INT8_MIN;
+        _min = INT8_MAX;
+        _count = 0;
     }
 
     void AppendElem(T elem) {
-
+        if (_count == _size) {
+            _size = _size * 4;
+            T* new_arr = new T[_size];
+            std::memcpy(new_arr, _array, _count*sizeof(int8_t));
+            delete [] _array;
+            _array = new_arr;
+        }
+        _array[_count] = elem;
+        _count++;
     }
 
     bool IsContains(T elem) {
@@ -18,15 +36,15 @@ public:
     }
 
     T GetMax() {
-        return 0;
+        return _max;
     }
 
     T GetMin() {
-        return 0;
+        return _min;
     }
 
     ~ArrayHandler() {
-
+        delete [] _array;
     }
 
 };
