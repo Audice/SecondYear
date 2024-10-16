70a95a7b85bc44d68164fb869b65101ae259f2a8
AlinaMiti <144599971+AlinaMiti@users.noreply.github.com>
2024-10-16 14:02:27 +0300

    Update main.cpp

--- BitField-2/main.cpp
+++ BitField-2/main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
+#include "Set.h"
+#include "BitField.h"
+#include <vector>
 
 
 int main() {
-    uint16_t a = 1;
+    /*uint16_t a = 1;
     //a = a << 10;
     std::cout << a << std::endl;
     uint8_t b = 0;
     b = static_cast<uint8_t>(a);
     std::cout << static_cast<uint32_t>(b) << std::endl;
+    */
+    size_t size = 10;
+    Set m(size);
+    for (size_t i = 0; i < size; i++)
+        m.InsertElem(i);
+    std::vector vec = m.GetPrimary();
+    for (size_t i = 0; i < vec.size(); i++)
+        std::cout << vec[i] << std::endl;
+
+
     return 0;
 }
