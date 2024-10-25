4ef2a3ab8cf94feae169403ed32e3e8e19208185
Karchkov Denis <karchkov.denis@mail.ru>
2024-10-25 10:02:31 +0300

    Correct bitfield

--- BitField-2/Set.cpp
+++ BitField-2/Set.cpp
@@ -1,1 +1,56 @@
 #include "Set.h"
+
+
+Set::Set(size_t mp) : _bitField(10) {
+
+}
+Set::Set(const Set &s) : _bitField(10){
+
+} 
+Set::Set(const BitField &bf) : _bitField(10){
+
+}
+
+size_t Set::GetMaxPower() const{
+    return 0;
+}    
+void Set::InsElem(const uint64_t Elem){
+
+}
+void Set::DelElem(const uint64_t Elem){
+
+}
+bool Set::IsMember(const uint64_t Elem) const{
+    return 0;
+}
+
+
+bool Set::operator== (const Set &s) const{
+    return 0;
+}
+bool Set::operator!= (const Set &s) const{
+    return 0;
+}
+Set& Set::operator=(const Set &s){
+    return *this;
+}
+Set Set::operator+ (const uint64_t Elem){
+    return *this;
+}
+                                  
+Set Set::operator- (const uint64_t Elem){
+    return *this;
+}
+                                   
+Set Set::operator+ (const Set &s){
+    return *this;
+}
+Set Set::operator* (const Set &s){
+    return *this;
+}
+Set Set::operator~ (){
+    return *this;
+}
+std::vector<uint64_t> Set::GetPrimary(){
+    return std::vector<uint64_t>();
+}
--- BitField-2/Set.h
+++ BitField-2/Set.h
@@ -5,28 +5,30 @@
 class Set
 {
 private:
-  int _maxPower;       // максимальная мощность множества
+  size_t _maxPower;       // максимальная мощность множества
   BitField _bitField; // битовое поле для хранения характеристического вектора
 public:
-  Set(int mp);
+  Set(size_t mp);
   Set(const Set &s);       // конструктор копирования
   Set(const BitField &bf); // конструктор преобразования типа
-  operator BitField();      // преобразование типа к битовому полю
+  operator BitField(){ // преобразование типа к битовому полю
+
+  }
   // доступ к битам
-  int GetMaxPower(void) const;     // максимальная мощность множества
-  void InsElem(const int Elem);       // включить элемент в множество
-  void DelElem(const int Elem);       // удалить элемент из множества
-  int IsMember(const int Elem) const; // проверить наличие элемента в множестве
+  size_t GetMaxPower(void) const;     // максимальная мощность множества
+  void InsElem(const uint64_t Elem);       // включить элемент в множество
+  void DelElem(const uint64_t Elem);       // удалить элемент из множества
+  bool IsMember(const uint64_t Elem) const; // проверить наличие элемента в множестве
   // теоретико-множественные операции
-  int operator== (const Set &s) const; // сравнение
-  int operator!= (const Set &s) const; // сравнение
+  bool operator== (const Set &s) const; // сравнение
+  bool operator!= (const Set &s) const; // сравнение
   Set& operator=(const Set &s);  // присваивание
-  Set operator+ (const int Elem); // объединение с элементом
+  Set operator+ (const uint64_t Elem); // объединение с элементом
                                    // элемент должен быть из того же универса
-  Set operator- (const int Elem); // разность с элементом
+  Set operator- (const uint64_t Elem); // разность с элементом
                                    // элемент должен быть из того же универса
   Set operator+ (const Set &s);  // объединение
   Set operator* (const Set &s);  // пересечение
-  Set operator~ (void);           // дополнение
+  Set operator~ ();           // дополнение
   std::vector<uint64_t> GetPrimary(); // Выдать простые числа множества
 };
