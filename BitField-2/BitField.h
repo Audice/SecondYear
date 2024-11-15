f23f36a3c8668ccf6278adb6dbc081316be49f50
Kamil Fatekhov <113035726+Kamil-Fatekhov@users.noreply.github.com>
2024-11-15 04:33:14 +0300

    Update BitField.h

--- BitField-2/BitField.h
+++ BitField-2/BitField.h
@@ -1,4 +1,7 @@
+#pragma once
 #include <iostream>
+#include <stdint.h>
+using namespace std;
 
 class BitField {
 private:
@@ -12,18 +15,20 @@ public:
     BitField(size_t len);
     BitField(const BitField& tmp);
     BitField& operator=(const BitField& tmp);
-    
-    size_t GetLength() const; // Получить количество бит
+
+    size_t GetLength() const;
     void SetBit(size_t n);
-    void ClrBit(size_t n); // Настя
-    uint8_t GetBit(size_t n) const; // Саша
+    void ClrBit(size_t n);
+    uint8_t GetBit(size_t n) const;
 
 
-    BitField operator|(const BitField& tmp); // Карина
-    BitField operator&(const BitField& tmp); // Алина
-    BitField operator^(const BitField& tmp); // Арина
-    bool operator==(const BitField& tmp); // Даша
-    BitField operator~(); // Все
+    BitField operator|(const BitField& tmp);
+    BitField operator&(const BitField& tmp);
+    BitField operator^(const BitField& tmp);
+    bool operator==(const BitField& tmp) const;
+    BitField operator~();
 
-    ~BitField(); // Все
+    ~BitField() {
+
+    }
 };
